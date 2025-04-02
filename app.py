351f5a1a9f29357207a166e649a36d6f4b2957a8
HarshalPatil02 <harshalp081@gmail.com>
2025-04-02 16:40:40 +0530

    Update app.py
    Signed-off-by: HarshalPatil02 <harshalp081@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
