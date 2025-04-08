c1969f0e873dc7d528e9a1b129415cc703829164
HarshalPatil02 <harshalp081@gmail.com>
2025-04-08 11:41:46 +0530

    Update app1.py
    Signed-off-by: HarshalPatil02 <harshalp081@gmail.com>

--- app1.py
+++ app1.py
@@ -16,12 +16,12 @@ uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx"])
 
 if uploaded_file is not None:
     try:
-    df = pd.read_excel(uploaded_file, engine="openpyxl")
-    if df.empty:
-        st.error("Uploaded file is empty. Please upload a valid Excel file.")
-    else:
-        st.subheader("Uploaded Data Preview")
-        st.dataframe(df)
+        df = pd.read_excel(uploaded_file, engine="openpyxl")
+        if df.empty:
+            st.error("Uploaded file is empty. Please upload a valid Excel file.")
+        else:
+            st.subheader("Uploaded Data Preview")
+            st.dataframe(df)
     except Exception as e:
         st.error(f"An error occurred while reading the file: {e}")
 
