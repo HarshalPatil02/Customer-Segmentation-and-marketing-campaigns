4e0a867962f633fa324de1749c053881c66e9c90
HarshalPatil02 <harshalp081@gmail.com>
2025-04-08 11:35:20 +0530

    Update app1.py
    Signed-off-by: HarshalPatil02 <harshalp081@gmail.com>

--- app1.py
+++ app1.py
@@ -178,3 +178,6 @@ if st.checkbox("Show Train-Test Split Evaluation for K-Means"):
     
     fig = px.bar(df_spending, x="Product Category", y="Total Spending", title="Total Spending by Product Category", color="Product Category")
     st.plotly_chart(fig)
+
+else:
+    st.warning("Please upload an Excel file to proceed.")
